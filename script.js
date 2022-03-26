f4511fa8d16553c26340a67892f839d8ddcf75c9
Tarik Souabny <75922626+tariksouabny@users.noreply.github.com>
2022-03-26 07:55:56 -0400

    Initialized event listener for slider

--- script.js
+++ script.js
@@ -8,15 +8,23 @@ if (debug) {
   );
 }
 
-const grid = document.querySelector('.etch-a-sketch');
-const gridBGColor = '#cccccc'
+const grid = document.querySelector(".etch-a-sketch");
+const gridBGColor = "#cccccc";
 let clearGrid = () => {
-  grid.childNodes.forEach(
-    element => {
-      if (element.style){
-      element.style.backgroundColor = gridBGColor
-      }
+  grid.childNodes.forEach((element) => {
+    if (element.style) {
+      element.style.backgroundColor = gridBGColor;
     }
-  )
-}
+  });
+};
 
+let numberOfColsRows = 5;
+
+const slider = document.querySelector("#slider");
+slider.addEventListener("input", (e) => {
+  numberOfColsRows = slider.value;
+});
+
+const changeNumRowsCols = (numRowsCols) => {
+  clearGrid();
+};
